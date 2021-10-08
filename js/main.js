f02803f5aec0aa07dd0b66866ee88b50353ae692
Rajankdk <69184716+Rajankdk@users.noreply.github.com>
2021-10-08 15:02:06 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -339,20 +339,6 @@
 
 }
 
-setInterval(function() { makeTimer(); }, 1000);
-	
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-}
-
-
-
 
 })(jQuery);
 
